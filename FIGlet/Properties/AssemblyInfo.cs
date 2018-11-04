3243f520e450ab1cc3a121bcd2037da3741615e7
Pascal Craponne <pascal@craponne.fr>
2018-11-04 10:00:49 +0100

    Updated description (again)

--- FIGlet/Properties/AssemblyInfo.cs
+++ FIGlet/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("MIT license https://opensource.org/licenses/MIT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyDescription("A FIGlet library that allows to generate ASCII art (in the form of big letters) from text.")]
+[assembly: AssemblyDescription("A FIGlet library that allows to generate ASCII art (in the form of big letters) from text. Supports most common features such as fitting and smushing, reads fonts from raw or zipped files and offers extensibility.")]
 
 [assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.1")]
